ac1af694dffe527df2c66db6f03b86c32cde5515
divykansara <109065926+divykansara@users.noreply.github.com>
2022-07-12 14:29:20 +0530

    natural number or not

---
+++ natural_num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+
+    if(n>=1){
+        printf("natural number");
+
+    }
+    else{
+        printf("not a natural number ");
+
+    }
+    return 0;
+
+}

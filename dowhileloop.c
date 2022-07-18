516052615cce5e81b55c21f04a456e1d722d20b4
divykansara <109065926+divykansara@users.noreply.github.com>
2022-07-18 16:44:22 +0530

    0 to n numbers

--- dowhileloop.c
+++ dowhileloop.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int i=10;
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    int i=0;
+
     do{
         printf("%d  ",i);
-        i--;
-    }while(i>=00);
+
+        i++;
+    }while(i<=n);
     return 0;
 
     
